231b601fbaa2a5eee2087dc90f8f078bc2d5c40d
Jasper <jasperjian@outlook.co.nz>
2023-09-21 15:49:33 +1200

    dynamic appstate.json path

--- controllers/reader.js
+++ controllers/reader.js
@@ -2,24 +2,33 @@ const fs = require('fs')
 const path = require('path')
 
 const reader = async (req, res) => {
-  const cwd = process.cwd()
-  const filePath = path.join(cwd, '/data/appState.json')
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error('Error reading the JSON file:', err)
-      res.status(500).json({ error: 'Internal server error' })
-      return
-    }
+  // const cwd = process.cwd()
+  // const filePath = path.join(cwd, 'AppData', 'Roaming')
 
-    try {
-      const jsonData = JSON.parse(data)
-      res.json(jsonData)
-      console.log('connected to reader controller')
-    } catch (parseError) {
-      console.error('Error parsing JSON:', parseError)
-      res.status(500).json({ error: 'Internal server error' })
-    }
-  })
+  const userProfile = process.env.USERPROFILE
+
+  if (userProfile) {
+   const dynamicPath = path.join(userProfile, 'AppData', 'Roaming', 'HowickHLCv3', 'appState.json')
+   console.log('dynamic path: ', dynamicPath)
+     fs.readFile(dynamicPath, 'utf8', (err, data) => {
+       if (err) {
+         console.error('Error reading the JSON file:', err)
+         res.status(500).json({ error: 'Internal server error' })
+         return
+       }
+
+       try {
+         const jsonData = JSON.parse(data)
+         res.json(jsonData)
+         console.log('connected to reader controller')
+       } catch (parseError) {
+         console.error('Error parsing JSON:', parseError)
+         res.status(500).json({ error: 'Internal server error' })
+       }
+     })
+  }
+
+
 }
 
 const readerController = { reader }
