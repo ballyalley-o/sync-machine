f572aef0272b9c2aa2fa19448ee0d8905f2d9555
Jasper <jasperjian@outlook.co.nz>
2023-10-16 15:26:38 +1300

    analog looper middleware

---
+++ middleware/analog-looper.js
@@ -0,0 +1,18 @@
+const  logger  = require("./logger")
+
+const analogLooper = (appState) => {
+   if (!appState) return logger.error('No Appstate provided')
+
+   try {
+     const analogParam = appState.config.includes('Analogue_Display_')
+     console.log(appState.config.hasOwnProperty('Analogue_Display_0'), 'isAnalogProps')
+
+     return analogParam
+   } catch (err) {
+    logger.error(err)
+   }
+
+   return ''
+}
+
+module.exports = analogLooper
