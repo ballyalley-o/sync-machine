bd667fed6ff2199ecd4b43dc6eb1efe032056b0a
ballyalley-o <bally.lomibao@gmail.com>
2023-10-26 11:31:23 +1300

    reconfig url with absolute func

--- constants/url.js
+++ constants/url.js
@@ -1,22 +1,37 @@
 const PATH = require('./path')
 const {GLOBAL} = require('../config')
 
+
+const API = GLOBAL.api
+
+
+const absPath = (...params) => {
+  return API + params.join('/')
+}
+
+
 const LOG = {
-  coil: '/coil',
-  coil_parsed: '/parsed-coil-log',
+  coil: 'coil',
+  coil_parsed: 'parsed-coil-log',
 }
 
 
 const APPSTATE ={
-  extract: '/extract'
+  extract: 'extract',
+  latest: 'latest',
+  win: 'win'
 }
 
 const URL = {
   log: {
-    erp: PATH.log + LOG.coil_parsed,
+    coil: absPath(PATH.log, LOG.coil),
+    coil_parsed: absPath(PATH.log, LOG.coil_parsed)
   },
   app_state: {
-    extract: GLOBAL.api + PATH.app_state + APPSTATE.extract,
+    this: absPath(PATH.app_state),
+    extract: absPath(PATH.app_state, APPSTATE.extract),
+    latest: absPath(PATH.app_state, APPSTATE.latest),
+    win: absPath(PATH.app_state, APPSTATE.win)
   },
 }
 
