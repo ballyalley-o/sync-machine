366209c247733ac449abd1ef2f0ae80ca769fa80
ballyalley-o <bally.lomibao@gmail.com>
2023-10-27 15:51:36 +1300

    reconfigured logger, removed optional

--- middleware/logger.js
+++ middleware/logger.js
@@ -3,10 +3,10 @@ require('colors')
 const logger = {
   custom: (message, color, optional) =>
     console.log(message[color], optional || null),
-  info: (message, optional) =>
-    console.log(message.bgBlue, optional),
+  info: (message) =>
+    console.log(message.bgBlue),
   error: (message, optional) => console.log(message.bgRed, optional || null),
-  log: (message, optional) => console.log(message.yellow),
+  log: (message) => console.log(message.yellow),
   warn: (message, optional) => console.log(message.bgYellow, optional || null),
 }
 
