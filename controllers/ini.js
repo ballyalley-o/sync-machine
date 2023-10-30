99f5f03a6742a8413df1cf0f10feac742f168910
ballyalley-o <bally.lomibao@gmail.com>
2023-10-30 16:18:04 +1300

    ini controller with ini sim mode controller

--- controllers/ini.js
+++ controllers/ini.js
@@ -5,12 +5,13 @@ const fs = require('fs')
 const { logger } = require('../middleware')
 const { paths } = require('../utils')
 const { GLOBAL } = require('../config')
+const { BURN_IN_PARAMS, RESPONSE } = require('../constants')
 
 const USERPROFILE = GLOBAL.userProfile
 
 // @desc extract framesSet
-// @path /api/v1/app-state/frames
-// @access Public [not implemented]
+// @path /api/0.0.1/ini
+// @access Private - Dev [not implemented]
 const iniExtract = async (req, res) => {
   if (USERPROFILE) {
     // await paths.rootPath('howick', 'ini').then((result) => {
@@ -44,5 +45,113 @@ const iniExtract = async (req, res) => {
 }
 
 
-const iniController = {iniExtract}
+// @desc prepare ini for Simulation
+// @path /api/0.0.1/ini/sim
+// @access Private - Dev [not implemented]
+const iniSimulation = async (req, res) => {
+  if (USERPROFILE) {
+    fs.readFile(paths.testFilesPath, 'utf8', (err, data) => {
+      if (err) {
+        res.status(500).json({ error: err.message })
+        return
+      }
+      try {
+        let lines = data.split('\n')
+        const modifiedLines = []
+
+        const prevIni = []
+        prevIni.push(lines)
+
+        for (const line of lines) {
+          let modifiedLine = line
+
+          const isBurnIn = line.includes(BURN_IN_PARAMS.burnIn)
+          if (isBurnIn) {
+            const parts = line.split('=')
+            if (parts.length === 2) {
+              const boolValue = JSON.parse(parts[1].trim())
+
+              // convert burnin to true
+              if (boolValue == false) {
+                parts[1] = 'true'
+                modifiedLine = parts.join('=')
+              }
+            }
+          }
+
+          // change target window value to 20
+          const targetWindow = line.match(BURN_IN_PARAMS.targetWindow)
+          if (targetWindow) {
+            const parts = line.split('=')
+            if (parts.length === 2) {
+              const targetWinValue = parseFloat(parts[1].trim())
+
+              // convert burnin to true
+              if (targetWinValue < 20) {
+                parts[1] = 20
+                modifiedLine = parts.join('=')
+              }
+            }
+          }
+
+          // change time param to 300 if not already
+          const time = line.match(BURN_IN_PARAMS.time)
+          if (time) {
+            const parts = line.split('=')
+            if (parts.length === 2) {
+              const timeValue = parseFloat(parts[1].trim())
+
+              // convert burnin to true
+              if (timeValue < 300) {
+                parts[1] = 300
+                modifiedLine = parts.join('=')
+              }
+            }
+          }
+
+          //  change the values for proxes and solenoids
+          for (const param of BURN_IN_PARAMS.zeros) {
+            const PARAM = line.includes(param)
+
+            if (PARAM) {
+              // Split the line to separate the parameter name and its value
+              const parts = line.split('=')
+              if (parts.length === 2) {
+                // Trim and check if it's a valid number
+                const value = parseFloat(parts[1].trim())
+                if (!isNaN(value) && value !== 0) {
+                  // Change the value to 0
+                  parts[1] = '0'
+                  modifiedLine = parts.join('=')
+                }
+              }
+            }
+          }
+          modifiedLines.push(modifiedLine)
+        }
+        // Join the modified lines back into a single string
+        const modifiedData = modifiedLines.join('\n')
+        // const parsedModData = JSON.stringify(modifiedData)
+        // Write the modified data back to the file
+        fs.writeFile(paths.testFilesPath, modifiedData, (writeErr) => {
+          if (writeErr) {
+            res.status(500).json({ error: writeErr.message })
+          } else {
+            res.status(201).json({
+              message: RESPONSE.iniSimulation,
+              params: modifiedData,
+            })
+          }
+        })
+      } catch (err) {
+        logger.error(err)
+        res.status(500).json({ error: err.message })
+      }
+    })
+  }
+}
+
+
+
+const iniController = { iniExtract, iniSimulation }
 module.exports = iniController
