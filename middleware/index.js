fd3f1903a8c9a5909ed9ada67b09ec4a7cec12b9
ballyalley-o <bally.lomibao@gmail.com>
2023-10-31 15:50:21 +1300

    ini looper refactor

--- middleware/index.js
+++ middleware/index.js
@@ -2,6 +2,7 @@ module.exports = {
   logger: require('./logger'),
   logLooper: require('./logLooper'),
   coilLooper: require('./loopers/coil'),
+  iniLooper: require('./loopers/ini'),
   asyncHandler: require('./async-handler'),
   analogLooper: require('./analog-looper'),
 }
