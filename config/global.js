62617003bced210b12cda025e78f43a017ac3a60
ballyalley-o <bally.lomibao@gmail.com>
2023-11-01 16:35:15 +1300

    global re-configured

--- config/global.js
+++ config/global.js
@@ -1,12 +1,24 @@
-const {USER} = require('./user')
+const moment = require('moment-timezone')
+const { USER, NODE } = require('./user')
+const { COUNTRY } = require('../constants')
 const dotenv = require('dotenv')
 dotenv.config()
 
+const dateNow = new Date()
+
 const GLOBAL = {
   userProfile: USER,
+  node: NODE,
   api: process.env.API_LOCAL,
   apiRoot: process.env.API_ROOT,
   port: process.env.API_PORT,
+  time: {
+    UTC: dateNow.toUTCString(),
+    ISO: dateNow.toISOString(),
+    TIME: dateNow.toTimeString(),
+    DATE: dateNow.toLocaleDateString(),
+    custom: (city) => moment().tz(COUNTRY[city]).format(),
+  },
 }
 
 module.exports = GLOBAL
