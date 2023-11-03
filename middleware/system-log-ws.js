659df121f026fa5a61dcee36403775b71c3f8143
ballyalley-o <bally.lomibao@gmail.com>
2023-11-03 15:24:03 +1300

    websockets middleware

---
+++ middleware/system-log-ws.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+const fetch = require('node-fetch')
+const WebSocket = require('ws')
+const { logger, sysLooper } = require('../middleware')
+
+const sysLogWs = (server, port) => {
+  const wws = new WebSocket.Server({ server })
+
+  wws.on('connection', (ws) => {
+    const filePath = paths.livePath('sys', 'txt').then((result) => {
+      promisePath = result
+      logger.log(promisePath)
+      return promisePath
+    })
+
+    const stream = fs.createReadStream(filePath, { encoding: 'utf8' })
+
+    // read log and sends updates to websocket clients
+    stream.on('data', (data) => {
+      const lines = data.split('\n').reverse()
+      const dateLog = sysLooper(lines, 'date')
+      const sysLog = sysLooper(lines, 'log')
+
+      const response = {
+        dateLog,
+        sysLog,
+      }
+
+      ws.send(JSON.stringify(response))
+    })
+
+    stream.on('error', (error) => {
+      logger.error(error)
+    })
+
+    ws.on('close', () => {
+      // clean up after disconnecting websockets
+      stream.destroy()
+    })
+  })
+}
+
+
+module.exports = sysLogWs
