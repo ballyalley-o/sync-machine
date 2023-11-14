a4fbcd0d401e95d9921086e208ceb81b7e45511b
ballyalley-o <bally.lomibao@gmail.com>
2023-11-14 14:32:58 +1300

    extract middleware

--- middleware/extract.js
+++ middleware/extract.js
@@ -1,13 +1,7 @@
 function extractBySection(arr, section) {
-  const prevPropsArray = []
-  const modPropsArray = []
-  const profileArray = []
   const changes = []
   const paramArray = []
-  const iniArray = []
   const properties = {}
-  const keyValueArray = []
-  let keyValue
 
   if (!Array.isArray(arr)) {
     let arrLines = arr.split('\n')
@@ -19,53 +13,36 @@ function extractBySection(arr, section) {
   let currentSection = null
   // get the param for the properties : i.e. [MachineParameters]
   for (const line of arr) {
-    if (line.trim().startsWith('[') && line.trim().endsWith(']')) {
-      currentSection = line.trim().slice(1, -1)
-      paramArray.push(currentSection)
+      if (line.trim().startsWith('[') && line.trim().endsWith(']')) {
+        currentSection = line.trim().slice(1, -1)
+        paramArray.push(currentSection)
 
-      // const [key, value] = line.split('=')
+        // this takes out all the properties of each section
+        } else if (currentSection.includes(section)) {
 
-      // if (key && value) {
-      //   keyValue = [key, value]
-      //   keyValueArray.push(keyValue)
-      //   properties[key.trim()] = value.trim()
-      // }
+        const [key, value] = line.split('=')
 
-      // this takes out all the properties of each section
-    } else if (currentSection.includes(section)) {
+        // if Profile_1 just push the key and value
+        if (key && value) {
+            const keyTrimmed = key.trim()
+            const valueTrimmed = value.trim()
 
-      const [key, value] = line.split('=')
+            if (!properties[currentSection]) {
+                properties[currentSection] = []
+            }
+          const existingKeyValue = properties[currentSection].find(item => {
+            const popKeys = Object.keys(item).pop().trim()
+            popKeys === keyTrimmed
+          })
+          if (existingKeyValue) {
+            existingKeyValue[keyTrimmed] = valueTrimmed
 
-      // if Profile_1 just push the key and value
-      if (key && value) {
-          const keyTrimmed = key.trim()
-          const valueTrimmed = value.trim()
-
-          if (!properties[currentSection]) {
-              properties[currentSection] = []
+          } else {
+        // if key-value pair length is not more than 1 under a section, dont push it, isntead,
+        //  make it a value of the currentSection
+            properties[currentSection].push({[keyTrimmed]: valueTrimmed})
           }
-        const existingKeyValue = properties[currentSection].find(item => {
-          const popKeys = Object.keys(item).pop().trim()
-          popKeys === keyTrimmed
-        })
-        if (existingKeyValue) {
-          existingKeyValue[keyTrimmed] = valueTrimmed
-
-        } else {
-          // if key-value pair length is not more than 1 under a section, dont push it, just make it a value of the currentSection
-          properties[currentSection].push({[keyTrimmed]: valueTrimmed})
         }
-
-      }
-    } else if (currentSection === paramArray[0]) {
-      const [key, value] = line.split('=')
-
-        arr.forEach(section => {
-          if (key && value) {
-            properties[key.trim()] = value.trim()
-            iniArray.push(properties)
-          }
-        });
       }
     }
   return properties
