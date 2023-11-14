2c69acb310dcc8ffd03fcc0e2f201cb8b7321ce9
ballyalley-o <bally.lomibao@gmail.com>
2023-11-14 14:34:16 +1300

    🐎 ci(ini :section route added):

--- routes/ini.js
+++ routes/ini.js
@@ -10,6 +10,7 @@ router.get('/', iniController.iniExtract)
 router.get('/sim', iniController.iniSimulation)
 router.get('/compare', iniController.iniCompare)
 router.get('/extract', extractController.extractSection)
+router.get('/:section', extractController.extractDynamic)
 
 
 const iniRouter = router
