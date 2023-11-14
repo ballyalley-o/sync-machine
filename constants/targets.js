229c798600ff08b3970ed719689e1e32c67a6503
ballyalley-o <bally.lomibao@gmail.com>
2023-11-14 16:47:10 +1300

    🐎 ci(targets constants):

---
+++ constants/targets.js
@@ -0,0 +1,16 @@
+
+// TODO: add targets from params
+const TARGETS = [
+  'configurationformat',
+  'windowmode',
+  'profile',
+  'llcsocketdata',
+  'languageselect',
+  'textsizeselect',
+  'measurementsystem',
+  'machineparameters',
+  'llcparameters',
+  'label',
+]
+
+module.exports = TARGETS
