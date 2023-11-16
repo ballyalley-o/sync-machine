f362c8d59e5df579207b4e459c403214d06e77c5
ballyalley-o <bally.lomibao@gmail.com>
2023-11-16 16:17:47 +1300

    🎉 init(profile router):

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,21 @@
 const appStateRouter = require('./app-state.js')
 const iniRouter = require('./ini.js')
 const logRouter = require('./log.js')
+const profileRouter = require('./profile.js')
 // @globals
 const PATH = require('../constants/path.js')
 
 const appStateRoutes = appStateRouter
 const logRoutes = logRouter
 const iniRoutes = iniRouter
+const profileRoutes = profileRouter
 
 
 const mainRoute = (app) => {
   app.use(PATH.app_state, appStateRoutes)
   app.use(PATH.log, logRoutes)
   app.use(PATH.ini, iniRoutes)
+  app.use(PATH.profile, profileRoutes)
 }
 
 module.exports = mainRoute
