46b670830ee27140da8dbf75b76dfa0b0f34872f
ballyalley-o <bally.lomibao@gmail.com>
2023-12-18 13:38:44 +1300

    🦄 refactor(clean up in URL constant):

--- constants/url.js
+++ constants/url.js
@@ -14,11 +14,11 @@ const INI = {
 const LOG = {
   coil: 'coil',
   coil_parsed: 'parsed-coil-log',
+  erp: 'erp'
 }
 
 const APPSTATE ={
-  extract: 'extract',
-  latest: 'latest',
+  custom: 'custom',
   win: 'win',
   analog: 'analog',
   frames: 'frames'
@@ -32,16 +32,17 @@ const URL = {
   },
   log: {
     coil: absPath(PATH.log, LOG.coil),
-    coil_parsed: absPath(PATH.log, LOG.coil_parsed)
+    coil_parsed: absPath(PATH.log, LOG.coil_parsed),
+    erp: absPath(PATH.log, LOG.erp)
   },
   app_state: {
     this: absPath(PATH.app_state),
-    extract: absPath(PATH.app_state, APPSTATE.extract),
-    latest: absPath(PATH.app_state, APPSTATE.latest),
+    custom: absPath(PATH.app_state, APPSTATE.custom),
     win: absPath(PATH.app_state, APPSTATE.win),
     analog: absPath(PATH.app_state, APPSTATE.analog),
     frames: absPath(PATH.app_state, APPSTATE.frames),
   },
 }
+
 
 module.exports = URL
