f7fb74338edc0852d0b7673ffd35985d4c21aafd
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-12-18 17:04:43 +1300

    🌈 style(responses):

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,46 +1,41 @@
 const { GLOBAL } = require('../config')
-
 require('colors')
 
 const logger = {
   custom: (message, color, optional) =>
     console.log(message[color], optional || null),
-  info: (message) =>
-    console.log(message.bgBlue),
+  info: (message) => console.log(message.bgBlue),
   error: (message, error, optional) => {
-
-    console.error(message.bgRed, optional || null)},
+    console.error(message.bgRed, optional || null)
+  },
   log: (message) => console.log(message.yellow),
   warn: (message, optional) => console.log(message.bgYellow, optional || null),
   server: (port, isConnected) => {
-      const nodev = GLOBAL.node
-      const serv = {
-        NODE_v: nodev,
-        SERVER_PORT: port,
-        SERVER_STATUS: isConnected ? ' CONNECTED' : 'NO CONNECTION',
-      }
-      if (nodev >= 10) {
-          console.table(serv)
-      } else {
-        console.log('NODE v: '.bgYellow, serv.NODE_v.yellow )
-        console.log('SERVER_PORT: '.bgYellow, serv.SERVER_PORT.yellow )
-        console.log('SERVER_STATUS: '.bgYellow, serv.SERVER_STATUS.yellow)
-      }
-
+    const nodev = GLOBAL.node
+    const serv = {
+      NODE_v: nodev,
+      SERVER_PORT: port,
+      SERVER_STATUS: isConnected ? ' CONNECTED' : 'NO CONNECTION',
+    }
+    if (nodev >= 10) {
+      console.table(serv)
+    } else {
+      console.log('NODE v: '.bgYellow, serv.NODE_v.yellow)
+      console.log('SERVER_PORT: '.bgYellow, serv.SERVER_PORT.yellow)
+      console.log('SERVER_STATUS: '.bgYellow, serv.SERVER_STATUS.yellow)
+    }
   },
   db: (host, dbName, isConnected) => {
-    const DB_LOG =
-      {
-        HOST: host,
-        DATABASE: dbName,
-        STATUS: isConnected ? 'CONNECTED' : 'NO CONNECTION'
-      }
+    const DB_LOG = {
+      HOST: host,
+      DATABASE: dbName,
+      STATUS: isConnected ? 'CONNECTED' : 'NO CONNECTION',
+    }
 
-      console.log('HOST: ', DB_LOG.HOST.yellow)
-      console.log('DATABASE: ', DB_LOG.DATABASE.yellow)
-      // isConnected ? console.log('STATUS: ', DB_LOG.STATUS.green) :  console.log('STATUS: ', DB_LOG.STATUS.red)
-
-  }
+    console.log('HOST: ', DB_LOG.HOST.yellow)
+    console.log('DATABASE: ', DB_LOG.DATABASE.yellow)
+    isConnected ? console.log('STATUS: ', DB_LOG.STATUS.green) :  console.log('STATUS: ', DB_LOG.STATUS.red)
+  },
 }
 
 module.exports = logger
