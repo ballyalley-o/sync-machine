c2472813f3b586c21b3f5b0e483d9b81c1e5761b
ballyalley-o <bally.lomibao@gmail.com>
2023-12-18 15:09:17 +1300

    production route

--- routes/log.js
+++ routes/log.js
@@ -11,6 +11,7 @@ router.get('/coil', logController.coilLog)
 router.get('/parsed-coil-log', logController.parsedCoilLog)
 router.get('/sys', logController.sysLog)
 router.get('/erp', logController.erpLog)
+router.get('/production', logController.productionLog)
 
 const logRouter = router
 module.exports = logRouter
