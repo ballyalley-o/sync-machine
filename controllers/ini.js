65598f2ece2c6418208ca988e474c615e532f2f4
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-12-22 10:53:31 +1300

    change of logic in ini extract controller

--- controllers/ini.js
+++ controllers/ini.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const { parse, stringify } = require('ini')
 const { logger, iniLooper, extractBySectionObj } = require('../middleware')
 const { paths, compareActions, normalize } = require('../utils')
 const { GLOBAL } = require('../config')
@@ -11,24 +12,24 @@ const USERPROFILE = GLOBAL.userProfile
 // @access Private - Dev [not implemented]
 const extractIni = async (req, res) => {
   if (USERPROFILE) {
-
     fs.readFile(paths.iniPath, 'utf8', (err, data) => {
       if (err) {
         res.status(500).json({ error: err.message })
       }
       try {
-        const lines = data.split('\n')
+        // const lines = data.split('\n')
+        const lines = data.split('.')
+        const parsedIni = parse(data)
 
         let iniLines = []
 
         for (const line of lines) {
-             const equalSign = line.replace(/=/g, ':')
-             iniLines.push(equalSign)
-
+          const equalSign = line.replace(/=/g, ':')
+          iniLines.push(equalSign)
         }
 
         // parse ini to push to array
-        res.status(200).json(iniLines)
+        res.status(200).json(parsedIni)
       } catch (err) {
         logger.error(err)
         res.status(500).json({ error: err.message })
@@ -45,7 +46,7 @@ let modifiedIni = []
 // @access Private - Dev: Admin [not implemented]
 const simulationIni = async (req, res) => {
   if (USERPROFILE) {
-     fs.readFile(paths.iniPath, 'utf8', async (err, data) => {
+    fs.readFile(paths.iniPath, 'utf8', async (err, data) => {
       if (err) {
         res.status(500).json({ error: err.message })
         return
@@ -58,7 +59,11 @@ const simulationIni = async (req, res) => {
           let modifiedLine = line
 
           // change burn in param to true if not already
-          let burnInValue = await iniLooper.iniSimUpdate('burnIn', modifiedLine, line)
+          let burnInValue = await iniLooper.iniSimUpdate(
+            'burnIn',
+            modifiedLine,
+            line
+          )
           modifiedLine = burnInValue
 
           // change target value param to 20 if not already
@@ -92,39 +97,38 @@ const simulationIni = async (req, res) => {
           modifiedIni.push(modifiedLine)
         }
 
-          let modifiedData
-          // Join the modified lines back into a single string
-          if (data !== modifiedData) {
-            modifiedData = modifiedIni.join('\n')
-          }
+        let modifiedData
+        // Join the modified lines back into a single string
+        if (data !== modifiedData) {
+          modifiedData = modifiedIni.join('\n')
+        }
 
         //compare purposes only: to avoid duplicating the data
         let prevLength = data.length
         let modLength = modifiedData.length
 
-
         // Write the modified data back to the file
         if (data !== modifiedData) {
           const changes = compareActions.compareArr(data, modifiedData)
           if (changes.length >= 1) {
-             fs.writeFile(paths.iniPath, modifiedData, (writeErr) => {
-               if (writeErr) {
-                 res.status(500).json({ error: writeErr.message })
-               } else {
-                 modifiedIni = []
-                 res.status(201).json({
-                   message: RESPONSE.iniSimulation,
-                   params: changes,
-                   timestamp: GLOBAL.time.custom('akl'),
-                 })
-               }
-             })
+            fs.writeFile(paths.iniPath, modifiedData, (writeErr) => {
+              if (writeErr) {
+                res.status(500).json({ error: writeErr.message })
+              } else {
+                modifiedIni = []
+                res.status(201).json({
+                  message: RESPONSE.iniSimulation,
+                  params: changes,
+                  timestamp: GLOBAL.time.custom('akl'),
+                })
+              }
+            })
           } else {
-             res.status(200).json({
-               message: RESPONSE.noChanges,
-               params: [],
-               timestamp: GLOBAL.time.custom('akl'),
-             })
+            res.status(200).json({
+              message: RESPONSE.noChanges,
+              params: [],
+              timestamp: GLOBAL.time.custom('akl'),
+            })
           }
         } else {
           res.status(200).json({
@@ -152,16 +156,18 @@ const customIni = async (req, res) => {
         return res.status(500).json({ error: err.message })
       }
       // TODO: use a prompt to enter extract value
-        const linesOne = iniOne.split('\n')
+      const linesOne = iniOne.split('\n')
+      const lines = iniOne.split('.')
+      const parsedIni = parse(lines)
 
-        const EXTRACT_VALUE = '' // <- section to extract, default will extract all
-        const configuration = extractBySectionObj(linesOne, EXTRACT_VALUE)
+      const EXTRACT_VALUE = 'profile' // <- section to extract, default will extract all
+      const configuration = extractBySectionObj(parsedIni, EXTRACT_VALUE)
 
-        res.status(200).json({
-          message: RESPONSE.success[200],
-          success: true,
-          configuration
-        })
+      res.status(200).json({
+        message: RESPONSE.success[200],
+        success: true,
+        configuration,
+      })
     })
   } else {
     res.status(401).json({ error: RESPONSE.error[401] })
@@ -179,14 +185,21 @@ const dynamicIni = async (req, res, next) => {
 
   // handles dynamic concatinationbaltik09 of params with underscores
   if (lastUnderscoreIndex !== -1) {
-      slicedNumber = dynamicSection.slice(lastUnderscoreIndex + 1)
-      logger.info(slicedNumber)
-    } else {
-      logger.error(RESPONSE.error.underScoreIndex)
-    }
+    slicedNumber = dynamicSection.slice(lastUnderscoreIndex + 1)
+    logger.info(slicedNumber)
+  } else {
+    logger.error(RESPONSE.error.underScoreIndex)
+  }
 
-  const normalizedDynamicSection = await normalize.normalizeParam(dynamicSection, TARGETS)
-  const normalized = normalize.switchParam(param, normalizedDynamicSection, slicedNumber)
+  const normalizedDynamicSection = await normalize.normalizeParam(
+    dynamicSection,
+    TARGETS
+  )
+  const normalized = normalize.switchParam(
+    param,
+    normalizedDynamicSection,
+    slicedNumber
+  )
 
   if (dynamicSection) {
     if (USERPROFILE) {
@@ -202,13 +215,13 @@ const dynamicIni = async (req, res, next) => {
           message: RESPONSE.success[200],
           success: true,
           path: paths.iniPath,
-          section
+          section,
         })
       })
     } else {
       logger.error(RESPONSE.error.userProfile404(USERPROFILE))
       res.status(404).json({
-        error: RESPONSE.error[404]
+        error: RESPONSE.error[404],
       })
     }
   } else {
@@ -217,50 +230,51 @@ const dynamicIni = async (req, res, next) => {
   }
 }
 
-
 // TODO: UNDER DEVELOPMENT ======================================================
 
 // @desc compare ini
 // @path /api/0.0.1/ini/compare
 // @access Private - Dev [not implemented]
 const compareIni = async (req, res) => {
-    if (USERPROFILE) {
-      // using promisified callbacks from fs --not available for nodev9.11.1
-      // const iniOne =  fs.readFile(paths.iniPath, 'utf8')
-      // const iniTwo =  fs.readFile(paths.testFilesPath, 'utf8')
-      fs.readFile(paths.iniPath, 'utf8', (err, iniOne) => {
+  if (USERPROFILE) {
+    // using promisified callbacks from fs --not available for nodev9.11.1
+    // const iniOne =  fs.readFile(paths.iniPath, 'utf8')
+    // const iniTwo =  fs.readFile(paths.testFilesPath, 'utf8')
+    fs.readFile(paths.iniPath, 'utf8', (err, iniOne) => {
+      if (err) {
+        logger.error(err)
+        return res.status(500).json({ error: err.message })
+      }
+      // TODO: use user input path instead of this dev path
+      fs.readFile(paths.testFilesPath, 'utf8', (err, iniTwo) => {
         if (err) {
           logger.error(err)
-          return res.status(500).json({ error: err.message})
+          return res.status(500).json({ error: err.message })
         }
-// TODO: use user input path instead of this dev path
-        fs.readFile(paths.testFilesPath, 'utf8', (err, iniTwo) => {
-          if (err) {
-            logger.error(err)
-            return res.status(500).json({ error: err.message})
-          }
 
-          const linesOne = iniOne.split('\n')
-          const linesTwo = iniTwo.split('\n')
+        const linesOne = iniOne.split('\n')
+        const linesTwo = iniTwo.split('\n')
 
-// TODO: compare by param names instead of lines, add a different method: if the param has multiple include the tool names/parent name
-          const compare = compareActions.compareArrByProperty(
-            linesOne,
-            linesTwo
-          )
+        // TODO: compare by param names instead of lines, add a different method: if the param has multiple include the tool names/parent name
+        const compare = compareActions.compareArrByProperty(linesOne, linesTwo)
 
-          res.status(200).json({
-            message: 'Changes',
-            total: compare.length,
-            compare,
-          })
+        res.status(200).json({
+          message: 'Changes',
+          total: compare.length,
+          compare,
         })
       })
-    } else {
-      res.status(401).json({ error: 'Unauthorized' })
-    }
+    })
+  } else {
+    res.status(401).json({ error: 'Unauthorized' })
+  }
 }
 
-
-const iniController = { extractIni, simulationIni, compareIni, customIni, dynamicIni }
+const iniController = {
+  extractIni,
+  simulationIni,
+  compareIni,
+  customIni,
+  dynamicIni,
+}
 module.exports = iniController
