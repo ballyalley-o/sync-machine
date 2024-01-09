c9bd04a8ea0bb39b260dcb65215c95c8ce85000a
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-09 16:54:33 +1300

    erp log line indeces constant

--- constants/log.js
+++ constants/log.js
@@ -12,6 +12,23 @@ const LOG = {
     unit: 6,
     modClassifier: 7,
   },
+  erp: {
+    dateTime: 0,
+    operator: 1,
+    coilBatchName: 2,
+    coilWidth: 3,
+    coilThickness: 4,
+    coilLength: 5,
+    frameSet: 6,
+    componentLabel: 7,
+    web: 8,
+    flange: 9,
+    profileShape: 10,
+    componentLength: 11,
+    waste: 12,
+    time: 13,
+    componentWeight: 14,
+  },
 }
 
 module.exports = LOG
