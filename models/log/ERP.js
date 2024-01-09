38ef305ead5a222e5e596f537bd300b7c35e6449
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-09 16:55:38 +1300

    erp model change in field name, timestamps and collection name

--- models/log/ERP.js
+++ models/log/ERP.js
@@ -1,54 +1,60 @@
 const { Schema, model } = require('mongoose')
 
-const ERPSchema = new Schema({
-  dateTime: {
-    type: String,
-    required: true,
+const ERPSchema = new Schema(
+  {
+    dateTime: {
+      type: String,
+      required: true,
+    },
+    operator: {
+      type: String,
+    },
+    coilBatchName: {
+      type: String,
+    },
+    // coil/componentWidth
+    coilWidth: {
+      type: String,
+    },
+    coilThickness: {
+      type: String,
+    },
+    coilLength: {
+      type: String,
+    },
+    frameSet: {
+      type: String,
+    },
+    componentLabel: {
+      type: String,
+    },
+    web: {
+      type: String,
+    },
+    flange: {
+      type: String,
+    },
+    profileShape: {
+      type: String,
+    },
+    componentLength: {
+      type: String,
+    },
+    waste: {
+      type: String,
+    },
+    time: {
+      type: String,
+    },
+    componentWeight: {
+      type: String,
+    },
   },
-  operator: {
-    type: String,
-  },
-  coilBatchName: {
-    type: String,
-  },
-  // coil/componentWidth
-  coilWidth: {
-    type: String,
-  },
-  coilThickness: {
-    type: String,
-  },
-  coilLength: {
-    type: String,
-  },
-  frameSet: {
-    type: String,
-  },
-  componentLabel: {
-    type: String,
-  },
-  webWidth: {
-    type: String,
-  },
-  flangeHeight: {
-    type: String,
-  },
-  profileShape: {
-    type: String,
-  },
-  componentLength: {
-    type: String,
-  },
-  waste: {
-    type: String,
-  },
-  time: {
-    type: String,
-  },
-  componentWeight: {
-    type: String,
-  },
-})
+  {
+    timestamps: true,
+    collection: 'ERP',
+  }
+)
 
 const ERP = model('ERP', ERPSchema)
 module.exports = ERP
