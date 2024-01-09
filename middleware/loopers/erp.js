8315384b0c03c19ee609a322a62354d424b888f4
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-09 16:55:08 +1300

    erp looper

---
+++ middleware/loopers/erp.js
@@ -0,0 +1,29 @@
+const { LOG } = require('../../constants')
+
+const erpLooper = (lines, index) => {
+  const el = LOG.erp[index]
+  let result = {}
+
+  for (let i = 0; i < lines.length; i++) {
+    const splitEl = lines[i].includes(',')
+
+    if (splitEl) {
+      const lineArr = lines[i].split(',')
+
+      let parsedEl
+
+      // checking if the elements are string then just return it
+      if (typeof el === 'string') {
+        result[index] = lineArr[el].trim()
+      } else {
+        // for num types
+        parsedEl = lineArr[el]
+        result[index] = parsedEl.trim()
+      }
+    }
+  }
+
+  return result
+}
+
+module.exports = erpLooper
